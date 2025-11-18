8573874980604af010ad8956897198b3d53277d7
Rodrigo Teixeira <rodrigo.teixeira@metrobus.net>
2025-11-18 15:19:17 -0300

    Corrigir Alertas ao iniciar

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,8 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-// Bootstrap JS
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// Bootstrap JS (ESM build to avoid CommonJS warning)
+import 'bootstrap/dist/js/bootstrap.esm.js';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
